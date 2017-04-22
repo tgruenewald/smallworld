20eb73f42cd70a55cf337b2944207d0c6693e737
thomas School <thomas School>
2017-04-22 11:52:51 -0600

    planet grows, testy moves

---
+++ Assets/scripts/test_grow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_grow : MonoBehaviour {
+	float growsp = .01f;
+	//float kilsp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var size = GetComponent<Transform>();
+		if (Input.GetKey (KeyCode.G)) 
+		{
+			size.localScale = new Vector2 (size.localScale.x + growsp, size.localScale.y + growsp);
+		}
+		if (Input.GetKey (KeyCode.F)) 
+		{
+			size.localScale = new Vector2 (size.localScale.x - growsp, size.localScale.y - growsp);
+		}
+
+	}
+}
