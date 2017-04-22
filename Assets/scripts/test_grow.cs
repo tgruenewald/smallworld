c73cde3e4b522a15898456fd200f14c7152b84e7
Nimgoble <nimgoble@gmail.com>
2017-04-22 14:12:55 -0600

    Planets responding to player presence.

--- Assets/scripts/test_grow.cs
+++ Assets/scripts/test_grow.cs
@@ -3,42 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test_grow : MonoBehaviour {
-	float growsp = .01f;
-	float grvgrow = 10f;
-	//GameObject trig = GameObject.Find("Testy");
-	//float kilsp;
+	private static float growsp = .01f;
+	private static float grvgrow = 10f;
+    private bool shouldShrink = false;
+    private readonly Vector3 DefaultSizeVector = new Vector3(1.0f, 1.0f, 1.0f);
+    private readonly Vector3 GrowthSize = new Vector3(growsp, growsp, 0.0f);
+    private Transform SizeTransform = null;
+    private PointEffector2D GravityEffector = null;
 
-	// Use this for initialization
-	void Start () {
-
-	}
+    // Use this for initialization
+    void Start ()
+    {
+        SizeTransform = GetComponent<Transform>();
+        GravityEffector = GetComponent<PointEffector2D>();
+    }
 	void OnTriggerEnter2D(Collider2D Testy)
 	{ 
-		if (Testy.gameObject.tag != "player") 
-		{
+		if (Testy.gameObject.tag != "Player") 
 			return;
-		}
-		Debug.Log ("hi");
-		var size = GetComponent<Transform>();
-		var grav = GetComponent<PointEffector2D>();
-		size.localScale = new Vector2 (size.localScale.x + growsp, size.localScale.y + growsp);
-		grav.forceMagnitude -= grvgrow;
+        shouldShrink = true;
 	}
-	// Update is called once per frame
-	void Update () {
-		var size = GetComponent<Transform>();
-		var grav = GetComponent<PointEffector2D>();
-		if (Input.GetKey (KeyCode.G)) 
-		{
-			size.localScale = new Vector2 (size.localScale.x + growsp, size.localScale.y + growsp);
-			grav.forceMagnitude -= grvgrow;
-		}
-		if (Input.GetKey (KeyCode.F)) 
-		{
-			size.localScale = new Vector2 (size.localScale.x - growsp, size.localScale.y - growsp);
-			grav.forceMagnitude += grvgrow;
-		}
 
-	}
+    void OnTriggerExit2D(Collider2D Testy)
+    {
+        if (Testy.gameObject.tag != "Player")
+            return;
+        shouldShrink = false;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        if(shouldShrink)
+        {
+            SizeTransform.localScale -= GrowthSize;
+            GravityEffector.forceMagnitude -= grvgrow;
+        }
+        else if (SizeTransform.localScale != DefaultSizeVector)
+        {
+            SizeTransform.localScale += GrowthSize;
+            GravityEffector.forceMagnitude += grvgrow;
+        }
+    }
 
 }
