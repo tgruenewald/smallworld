10c1ab13760a57a95eebd8aa591ebf5fa7c4423b
tgruenewald <terrygruenewald@gmail.com>
2017-04-22 14:41:31 -0600

    including grow interior cs file

---
+++ Assets/scripts/grow_interior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grow_interior : MonoBehaviour {
+	float growsp = .01f;
+	float grvgrow = 20f;
+	//float kilsp;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var size = GetComponent<Transform>();
+		var grav = GetComponent<PointEffector2D>();
+		if (Input.GetKey (KeyCode.G)) 
+		{
+			size.localScale = new Vector2 (size.localScale.x + growsp, size.localScale.y + growsp);
+			grav.forceMagnitude -= grvgrow;
+		}
+		if (Input.GetKey (KeyCode.F)) 
+		{
+			size.localScale = new Vector2 (size.localScale.x - growsp, size.localScale.y - growsp);
+			grav.forceMagnitude += grvgrow;
+		}
+
+	}
+}
