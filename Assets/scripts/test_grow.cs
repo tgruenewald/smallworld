ccee7eb46f9abd1a3d8e49bf435be9db0f8e5ec5
thomas School <thomas School>
2017-04-22 13:49:36 -0600

    trigger

--- Assets/scripts/test_grow.cs
+++ Assets/scripts/test_grow.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class test_grow : MonoBehaviour {
 	float growsp = .01f;
 	float grvgrow = 10f;
+	//GameObject trig = GameObject.Find("Testy");
 	//float kilsp;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+	void OnTriggerEnter2D(Collider2D Testy)
+	{ 
+		if (Testy.gameObject.tag != "player") 
+		{
+			return;
+		}
+		Debug.Log ("hi");
+		var size = GetComponent<Transform>();
+		var grav = GetComponent<PointEffector2D>();
+		size.localScale = new Vector2 (size.localScale.x + growsp, size.localScale.y + growsp);
+		grav.forceMagnitude -= grvgrow;
+	}
 	// Update is called once per frame
 	void Update () {
 		var size = GetComponent<Transform>();
@@ -28,4 +40,5 @@ public class test_grow : MonoBehaviour {
 		}
 
 	}
+
 }
