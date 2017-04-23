9f66edae474dbdd600b8b0ab8d26cc83bbe26c26
Nimgoble <nimgoble@gmail.com>
2017-04-23 15:43:32 -0600

    Dialog Player

--- Assets/scripts/Interactions.cs
+++ Assets/scripts/Interactions.cs
@@ -1,301 +1,221 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.UI;
 
-public class Interactions : MonoBehaviour {
+public class Interactions : MonoBehaviour
+{
+    public Image textbg;
+    public Image textBorder;
+    public Image talkingHead;
 
-	public Image textbg;
-	public Image textBorder;
-	public Image talkingHead;
+    public Sprite radioImage;
+    public Sprite pcImage;
 
-	public Sprite radioImage;
-	public Sprite pcImage;
+    public Text textWithImage;
+    public Text storytext;
+    public bool dialogueComplete = false;
+    public bool inDialogue = false;
+    public bool printingText = false;
 
-	public Text textWithImage;
-	public Text storytext;
-	public bool dialogueComplete = false;
-	public bool inDialogue = false;
-	public bool printingText = false;
+    public string dialogueTag = "";
 
-	public string dialogueTag = "";
+    private float fadeInTime = 0.025F;
+    private bool keyPressed = false;
 
-	private float fadeInTime = 0.025F;
-	private bool keyPressed = false;
+    public int textStage = 0;
+    // Use this for initialization
+    private static Interactions interactions = null;
 
-	public int textStage = 0;
-	// Use this for initialization
-	private static Interactions interactions = null; 
+    private Dictionary<string, Dialog> Dialogs;
+    private Dictionary<string, Sprite> DialogSpriteCache;
 
-	void Start () {
-		//showText ("Here is some text.");
-	}
-	void Awake() {
-		DontDestroyOnLoad (gameObject);
-		if (interactions == null) {
-			interactions = this;
-		} else {
-			DestroyObject(gameObject);
-		}
-	}
+    private DialogPlayer OurDialogPlayer;
 
-	// Update is called once per frame
-	public void Update(){
-		if (Input.GetButtonDown ("Fire1")||Input.GetButtonDown("Jump")) {
-			//Debug.Log ("clicked");
+    private Text CurrentText;
 
-			if (printingText)
-			{
-				printingText = false;
-			}
-			else if (dialogueComplete&&!printingText) {
+    public string DialogScriptsDirectory = "Dialog";
 
-				//Debug.Log ("hiding text");
-				hideText ();
-				inDialogue = false;
-				dialogueComplete = false;
+    public Interactions()
+    {
+        Dialogs = new Dictionary<string, Dialog>();
+        DialogSpriteCache = new Dictionary<string, Sprite>();
+        //OurDialogPlayer = new DialogPlayer();
+        CurrentText = null;
+    }
 
-			} 			
-			else if(inDialogue&&!printingText)
-			{
-				showText(dialogueTag);
-			}
-		}//if click
-	}//if Update
+    void Start()
+    {
+        if (OurDialogPlayer == null)
+            OurDialogPlayer = this.gameObject.AddComponent<DialogPlayer>();
 
+        LoadDialogs();
+        CacheDialogSprites();
+        OurDialogPlayer.SectionStarted += this.SectionStarted;
+        OurDialogPlayer.SectionFinished += this.SectionFinished;
+        OurDialogPlayer.StartedPlaying += this.PlayerStarted;
+        OurDialogPlayer.FinishedPlaying += this.PlayerFinished;
+        OurDialogPlayer.TextUpdated += this.TextUpdated;
+    }
 
-	public void showText(string tag){
-		dialogueTag = tag;
-		//Debug.Log ("showing text: " + dialoguetext);
-		inDialogue = true;
+    private void SectionStarted(object sender, DialogSection section)
+    {
+        if (!string.IsNullOrEmpty(section.Icon))
+        {
+            if (DialogSpriteCache.ContainsKey(section.Icon))
+            {
+                talkingHead.sprite = DialogSpriteCache[section.Icon];
+                talkingHead.enabled = true;
+                storytext.enabled = false;
+                textWithImage.enabled = true;
+                CurrentText = textWithImage;
+            }
+            else
+            {
+                talkingHead.sprite = null;
+                talkingHead.enabled = false;
+                textWithImage.enabled = false;
+                storytext.enabled = true;
+                CurrentText = storytext;
+            }
+        }
+        else
+        {
+            talkingHead.sprite = null;
+            talkingHead.enabled = false;
+            textWithImage.enabled = false;
+            storytext.enabled = true;
+            CurrentText = storytext;
+        }
+    }
 
-		if(textManager (tag)==true){
-			talkingHead.enabled = true;
-		}
-		textBorder.enabled = true;
-		textbg.enabled = true;
+    private void SectionFinished(object sender, DialogSection section)
+    {
 
-		//StartCoroutine (animate (textManager(tag)));
+    }
 
-	}
+    private void PlayerStarted(object sender)
+    {
 
-	public void hideText (){
-		textBorder.enabled = false;
-		talkingHead.enabled = false;
-		textWithImage.enabled = false;
-		textbg.enabled = false;
-		storytext.enabled = false;
-	}
+    }
 
-	public IEnumerator animate(string strComplete, string speaker){
-		Text text;
-		if (speaker == "radio"){
-			text = textWithImage;
-			talkingHead.sprite = radioImage;
-		}
-		else if (speaker == "pc"){
-			text = textWithImage;
-			talkingHead.sprite = pcImage;
-		}
-		else{
-			text = storytext;
-		}
+    private void PlayerFinished(object sender)
+    {
 
-		printingText = true;
-		int i = 0;
-		text.text = "";
-		text.enabled = true;
-		while( i < strComplete.Length && printingText){
-			text.text += strComplete[i++];
-			yield return new WaitForSeconds(fadeInTime);
-		}
-		text.text = strComplete;
-		printingText = false;
+    }
 
+    private void TextUpdated(object sender, string updatedText)
+    {
+        if (CurrentText != null)
+            CurrentText.text = updatedText;
+    }
+    private void LoadDialogs()
+    {
+        var dialogScripts = Resources.LoadAll<TextAsset>(DialogScriptsDirectory);
+        foreach (var dialogScript in dialogScripts)
+        {
+            Dialog dialog = JsonUtility.FromJson<Dialog>(dialogScript.text);
+            if (dialog == null)
+                continue;
+            if (string.IsNullOrEmpty(dialog.Name))
+                continue;
 
-	}
+            Dialogs.Add(dialog.Name, dialog);
+        }
+    }
 
-	public bool textManager(string tag)
-	{
-		textStage++;
-		switch (tag) {
+    private void CacheDialogSprites()
+    {
+        HashSet<string> spritePaths = new HashSet<string>(Dialogs.SelectMany(x => from section in x.Value.Sections where !string.IsNullOrEmpty(section.Icon) select section.Icon));
+        foreach (var path in spritePaths)
+        {
+            Sprite loadedSprite = Resources.Load<Sprite>(path);
+            if (loadedSprite != null)
+                DialogSpriteCache.Add(path, loadedSprite);
+        }
+    }
 
-		case "lady":
-			switch (textStage) {
-			case 1:
-				StartCoroutine (animate ("Captain Arugua?", "pc"));
-				return false;
-			case 2:
-				StartCoroutine (animate ("Finally.", "radio"));
-				return false;
-			case 3:
-				StartCoroutine (animate ("What's going on? I was just attacked by a microwave!", "pc"));
-				dialogueComplete = true;
-				textStage = 0;
-				return false;		
-			}
-			return false;
-//			StartCoroutine (animate ("I am a lady talking to you.", ""));
-//			dialogueComplete = true;
-//			textStage = 0;
-//			return false;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (interactions == null)
+        {
+            interactions = this;
+        }
+        else
+        {
+            DestroyObject(gameObject);
+        }
+    }
 
-		case "radio1":
-			StartCoroutine (animate ("This is Captain Arugua. Can anyone hear me? Over.", "radio"));
-			dialogueComplete = true;
-			textStage = 0;
-			return true;
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
+        {
+            //Debug.Log ("clicked");
+            if (OurDialogPlayer.IsFinishedPlaying)
+            {
 
-		case "radio2":
-			StartCoroutine (animate ("Someone pick up this radio. Over.", "radio"));
-			dialogueComplete = true;
-			textStage = 0;
-			return true;
+                //Debug.Log ("hiding text");
+                hideText();
+                inDialogue = false;
+                dialogueComplete = false;
 
-		case "radio3":
-			switch (textStage) {
-				case 1:
-				StartCoroutine (animate ("Captain Arugua?", "pc"));
-					return true;
-				case 2:
-				StartCoroutine (animate ("Finally.", "radio"));
-					return true;
-				case 3:
-						StartCoroutine (animate ("What's going on? I was just attacked by a microwave!", "pc"));
-					return true;
-
-				case 4:
-				StartCoroutine (animate ("Something's gone wrong with the tech. Not just at HQ, but throughout the city. Over.", "radio"));
-					return true;
-
-				case 5:
-				StartCoroutine (animate ("I hope it's not aliens again. Or Chthulu.", "pc"));
-					return true;
-
-				case 6:
-				StartCoroutine (animate ("Whatever it is, we've triangulated the source of the problem at the National Aquarium. Over.", "radio"));
-					return true;
-
-				case 7:
-				StartCoroutine (animate ("I'm on my way.", "pc"));
-					return true;
-
-				case 8:
-				StartCoroutine (animate ("Get to the rooftop. We're sending in a helicopter to airlift you to the drop point. Over.", "radio"));
-					return true;
-
-				case 9:
-				StartCoroutine (animate ("Wait, was I supposed to have been saying \"Over\" this whole time?", "pc"));
-					return true;
-
-			case 10:
-				StartCoroutine (animate ("*distinct sound of facepalm* Just move it.", "radio"));
-				dialogueComplete = true;
-				textStage = 0;
-					return true;
-				default:
-					return true;
+            }
+        }//if click
+    }//if Update
 
 
-			}//radio3 switch
+    public void showText(string tag)
+    {
+        Dialog dialog = null;
+        if (!Dialogs.TryGetValue(tag, out dialog))
+        {
+            talkingHead.enabled = false;
+            textWithImage.enabled = false;
+        }
 
-		case "radio6":
-			switch(textStage)
-			{
-			case 1:
-				StartCoroutine (animate ("Where's everyone else?", "pc"));
-				return true;
-			case 2:
-				StartCoroutine (animate ("They're trapped in the elevator.", "radio"));
-				return true;
-			case 3:
-				StartCoroutine (animate ("Great. I'll wait for them. What's the ETA on the helicopter?", "pc"));
-				return true;
-			case 4: 
-				StartCoroutine (animate ("Should be arriving ... soon. Ish.", "radio"));
-				return true;
-			case 5:
-				StartCoroutine (animate ("Did you just say \"soonish\"?", "pc"));
-				return true;
-			case 6:
-				StartCoroutine (animate ("I said soon. Over.", "radio"));
-				return true;
-			case 7:
-				StartCoroutine (animate ("Right ...", "pc"));
-				dialogueComplete = true;
-				textStage = 0;
-				return true;
-			default: 
-				return true;
-			}//radio6
-		case "radio11":
-			switch (textStage)
-			{
-			case 1: 
-				StartCoroutine (animate ("So my GPS isn't working ...", "pc"));
-				return true;
-			case 2:
-				StartCoroutine (animate ("And?", "radio"));
-				return true;
-			case 3:
-				StartCoroutine (animate ("How do I get to the aquarium?", "pc"));
-				return true;
-			case 4:
-				StartCoroutine (animate ("You've never been?", "radio"));
-				return true;
-			case 5:
-				StartCoroutine (animate ("Sure. Once. On a field trip.", "pc"));
-				return true;
-			case 6:
-				StartCoroutine (animate ("It's on the corner of Portland and Denver.", "radio"));
-				return true;
-			case 7:
-				StartCoroutine (animate ("... and where are those streets?", "pc"));
-				return true;
-			case 8:
-				StartCoroutine (animate ("You don't know *static* unbelievable! *static* If you went down the fire escape, you're on Portland right now.", "radio"));
-				return true;
-			case 9: 
-				StartCoroutine (animate ("So ... Denver is ... which way?", "pc"));
-				return true;
-			case 10:
-				StartCoroutine (animate ("*static*", "radio"));
-				return true;
-			case 11:
-				StartCoroutine (animate ("Hello? Over?", "pc"));
-				return true;
-			case 12:
-				StartCoroutine (animate ("*static*", "radio"));
-				return true;
-			case 13:
-				StartCoroutine (animate ("Looks like I'm on my own.", "pc"));
-				dialogueComplete = true;
-				textStage = 0;
-				return true;
-			default: 
-				return true;
-			}//radio11
+        if (!OurDialogPlayer.LoadDialog(dialog))
+        {
+            talkingHead.enabled = false;
+            textWithImage.enabled = false;
+        }
+        else if (OurDialogPlayer.PlayDialog())
+        {
+            talkingHead.enabled = true;
+            storytext.enabled = false;
+        }
 
-		default:
-			StartCoroutine (animate ("textManager did not find string", ""));
-			return false;
 
-		}//tagswitch
-	}//textManager
+        //dialogueTag = tag;
+        ////Debug.Log ("showing text: " + dialoguetext);
+        //inDialogue = true;
 
-/*	public IEnumerator WaitInput(string text, Text textbox) {
-		keyPressed = false;
-		while(!keyPressed)
-		{
-			//Debug.Log ("waiting for input in coroutine");
-			if ((Input.GetButtonDown ("Fire1")||Input.GetButtonDown("Jump"))&& !printingText) {
-				keyPressed = true;
-				break;
-			}
-			yield return 0;
-		}
-		StartCoroutine (animate (text,textbox));
-		dialogueComplete = true;
-		inDialogue = false;
-	}*/
+        //if(textManager (tag)==true){
+        //	talkingHead.enabled = true;
+        //	storytext.enabled = false;
+        //}
+        //else
+        //{
+        //	talkingHead.enabled = false;
+        //	textWithImage.enabled = false;
+        //}
+        textBorder.enabled = true;
+        textbg.enabled = true;
+
+        //StartCoroutine (animate (textManager(tag)));
+
+    }
+
+    public void hideText()
+    {
+        textBorder.enabled = false;
+        talkingHead.enabled = false;
+        textWithImage.enabled = false;
+        textbg.enabled = false;
+        storytext.enabled = false;
+    }
 }
 
