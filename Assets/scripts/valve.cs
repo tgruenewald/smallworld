3a4cd50374fd45d146c5c2e2eb8d42b1b2308c3b
tgruenewald <terrygruenewald@gmail.com>
2017-04-23 14:09:32 -0600

    first pass at valves

---
+++ Assets/scripts/valve.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valve : MonoBehaviour {
+
+	public Animator animator;
+	bool isOpen = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void activate() {
+		isOpen = !isOpen;
+		if (isOpen) {
+			SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> ();
+			sr.sprite = Resources.Load<Sprite> ("open_valve.png");
+		} else {
+			SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> ();
+			sr.sprite = Resources.Load<Sprite> ("close_valve.png");			
+		}
+	}
+
+	public Animator getAnimation() {
+		return animator;
+	}
+}
---
+++ Assets/ten.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ten : MonoBehaviour {
+
+	public GameObject val;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log ("entering tendril trigger");
+		val.GetComponent<val> ().activate ();
+
+
+	}
+}
