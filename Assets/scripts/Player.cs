01ab6389e29b296c01bcf12fc229b698bc5aaeea
Nimgoble <nimgoble@gmail.com>
2017-04-23 16:09:52 -0600

    Committing because Valve/valve is weird.

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -20,12 +20,17 @@ public class Player : MonoBehaviour
     public GameObject CurrentPlanet { get { return currentPlanet; } }
     private GameObject currentPlanet;
 
+    private PointEffector2D gravityEffector;
+    private float initialGravityForce;
+
     // Use this for initialization
     void Start ()
     {
 		GroundCheck = GetComponent<CircleCollider2D>();
         Camera.main.GetComponent<SmoothCamera>().target = gameObject;
 		animator = GetComponent<Animator>();
+        gravityEffector = GetComponent<PointEffector2D>();
+        initialGravityForce = gravityEffector.forceMagnitude;
     }
 	
 	// Update is called once per frame
@@ -34,20 +39,29 @@ public class Player : MonoBehaviour
         PerformGroundCheck();
 		DEBUG_GROUNDED = IsGrounded;
 		animator.SetBool("isWalking", false);
-        if(currentPlanet != null)
+        if (currentPlanet != null)
         {
             this.transform.up = -(currentPlanet.transform.position - this.transform.position);
+            PlanetGravity currentPlanetGravity = currentPlanet.GetComponentInChildren<PlanetGravity>();
+            if (currentPlanetGravity != null)
+            {
+                this.gravityEffector.forceMagnitude = initialGravityForce * currentPlanetGravity.ForceScale;
+            }
+
             //Vector3 directionToCurrentPlanetCenter = currentPlanet.transform.position - this.transform.position;
             //Quaternion lookAtQuat = Quaternion.LookRotation(directionToCurrentPlanetCenter);
             ////this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, this)
             //this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y, (lookAtQuat * Quaternion.Euler(0, 0, 90)).z, this.transform.rotation.w);
         }
+        else
+            this.gravityEffector.forceMagnitude = initialGravityForce;
         
         if (Input.GetKey(KeyCode.D))
         {
             //GetComponent<Rigidbody2D>().AddForce(transform.right * Speed);
             transform.position += transform.right * (Speed * Time.deltaTime);
-			if (!facingRight) {
+			if (!facingRight)
+            {
 				Flip ();
 			}
 			animator.SetBool("isWalking", true);
@@ -65,7 +79,8 @@ public class Player : MonoBehaviour
         {
             //GetComponent<Rigidbody2D>().AddForce(-(transform.right) * Speed);
             transform.position -= transform.right * (Speed * Time.deltaTime);
-			if (facingRight) {
+			if (facingRight)
+            {
 				Flip ();
 			}
 			animator.SetBool("isWalking", true);
@@ -76,12 +91,14 @@ public class Player : MonoBehaviour
             //transform.position += transform.up * JumpStrength;
         }
 		// animation control
-		if (IsGrounded) {
+		if (IsGrounded)
+        {
 			animator.SetBool("isJumping", false);		
-		} else {
+		}
+        else
+        {
 			animator.SetBool("isJumping", true);
 		}
-
     }
 
     private void PerformGroundCheck()
@@ -96,6 +113,8 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.GetComponent<Planet>() == null)
+            return;
         if (isGrounded && currentPlanet != null)
             return;
         currentPlanet = collision.gameObject;
