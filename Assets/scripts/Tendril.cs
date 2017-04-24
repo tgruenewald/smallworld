334e56ea2e80ed6f8d1a0b66df6515d708ee6686
tgruenewald <terrygruenewald@gmail.com>
2017-04-23 18:34:09 -0600

    upper cased Tendril and Valve

---
+++ Assets/scripts/Tendril.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tendril : MonoBehaviour {
+
+	public GameObject valve;
+	bool hasExited = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log("entered");
+		
+		if (hasExited) {
+			Animator animator = gameObject.GetComponent<Animator> ();
+			animator.SetBool("play", true);
+			valve.GetComponent<Valve> ().activate ();			
+			hasExited = false;
+		}
+
+	}
+	
+	void OnTriggerExit2D(Collider2D col){
+		Debug.Log("exited");
+		Animator animator = gameObject.GetComponent<Animator> ();
+		animator.SetBool("play", false);		
+		hasExited = true;
+	}	
+}
