a01a7b72160ae191f3d5d754fe8d6ac395afa2e1
Nimgoble <nimgoble@gmail.com>
2017-04-23 21:09:31 -0600

    Fixed planet bounding boxes
    Added spawn point stuff to player

--- Assets/scripts/SpawnPoint.cs
+++ Assets/scripts/SpawnPoint.cs
@@ -4,23 +4,29 @@ using System.Collections;
 public class SpawnPoint : MonoBehaviour {
 
 	public AudioClip LevelAmbientSound;
+    public string SpawnPointName;
 
 	// Use this for initialization
 	void Start () {
-		var playerDroplet = GameState.GetPlayer();
-		if (playerDroplet != null)
+		var player = GameState.GetPlayer();
+		if (player != null)
 		{
-			//Debug.Log("Setting droplet start pos: " + GetComponent<Transform>().position);
-			//playerDroplet.StopAllAudio();
-			//playerDroplet.PlayAudio(LevelAmbientSound);
-			//playerDroplet.SpawnAt(this.gameObject);
+            if (player.NextTargetSpawnPoint != this.SpawnPointName)
+                return;
+            //Debug.Log("Setting droplet start pos: " + GetComponent<Transform>().position);
+            //playerDroplet.StopAllAudio();
+            //playerDroplet.PlayAudio(LevelAmbientSound);
+            //playerDroplet.SpawnAt(this.gameObject);
+            player.SpawnAt(this.gameObject);
 
-			var otherPlayers = GameObject.FindObjectsOfType<droplet>();
-			Debug.Log(string.Format("Amount of droplets in scene: {0}", otherPlayers.Length));
-			for(int i = 0; i < otherPlayers.Length; ++i){
+            var otherPlayers = GameObject.FindObjectsOfType<Player>();
+			Debug.Log(string.Format("Amount of player in scene: {0}", otherPlayers.Length));
+			for(int i = 0; i < otherPlayers.Length; ++i)
+            {
 				var otherPlayer = otherPlayers[i];
-				if(otherPlayer != playerDroplet){
-					Debug.Log(string.Format("Destroying object: {0} because it's not object: {1}", otherPlayer.gameObject.name, playerDroplet.gameObject.name));
+				if(otherPlayer != player)
+                {
+					Debug.Log(string.Format("Destroying object: {0} because it's not object: {1}", otherPlayer.gameObject.name, player.gameObject.name));
 					Destroy(otherPlayer.gameObject);
 				}
 			}
