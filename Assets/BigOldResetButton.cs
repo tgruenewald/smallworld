a2f500b8f6c71c699a60a494b63751a2d523748d
tgruenewald <terrygruenewald@gmail.com>
2017-04-24 22:01:45 -0600

    cleaned up junk and added abort button

---
+++ Assets/BigOldResetButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BigOldResetButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void onClick() {
+		Debug.Log ("============================================================");
+		SceneManager.LoadScene("outer_space"); 		
+	}
+}
