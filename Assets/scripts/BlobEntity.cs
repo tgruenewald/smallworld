3598898cf8f21a81195e6e1001b4832d10b2d3c1
Nimgoble <nimgoble@gmail.com>
2017-04-24 18:52:07 -0600

    Refactored.

---
+++ Assets/scripts/BlobEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobEntity : MonoBehaviour
+{
+
+    public AlienPickup Target;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        Target.GetComponent<SpriteRenderer>().enabled = true;
+        Target.GetComponent<CapsuleCollider2D>().enabled = true;
+    }
+}
